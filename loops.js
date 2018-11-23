d634c15f71c8ebfeb48b6322e7156903b0b5e4ff
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-23 19:03:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,9 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      i = "I am 1 strange loop"
+      "I am 1 strange loop"
     }
+    else
+      "I am #{i} strange loops"
   }
 }
